eea919a27e96b5ac297b67dfcc6132ff8dab6d6a
Kenna <kenna@graphcms.com>
2019-12-08 17:04:49 +0100

    does this fix it?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import illustration from "../images/illustration.png"
+// import illustration from "../images/illustration.png"
 import {
   Flex,
   Heading,
